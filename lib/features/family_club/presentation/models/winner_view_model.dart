a532fb7e8f47e56e9e87dda556acb1eac0658ffc
Francisco Javier Galán Guerrero <fjgalan@coviran.es>
2024-03-21 13:07:02 +0100

    chore: change name of file and model

--- lib/features/family_club/presentation/models/winner_view_model.dart
+++ lib/features/family_club/presentation/models/winner_view_model.dart
@@ -1,9 +1,9 @@
-class WinnerModel {
+class WinnerViewModel {
   final int date;
   final String cardNumber;
   final String prize;
 
-  WinnerModel({
+  WinnerViewModel({
     required this.date,
     required this.cardNumber,
     required this.prize,
